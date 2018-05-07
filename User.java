6c1a504152c43d2615f9afda96215b64130be374
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2018-05-07 15:02:50 +1000

    User Class for Merging

--- User.java
+++ User.java
@@ -1,13 +1,44 @@
-public abstract class User {
-    private String id;
-    private String password ;
-    private String firstName;
-    private String lastName ;
 
-    public User(String id, String password, String firstName, String lastName) {
-        this.id = id;
-        this.password = password;
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+public abstract class User {
+	private String id,firstName,lastName, password;
+
+	public User(String id,String firstName, String lastName, String password) {		
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName  = lastName;
+		this.password = password;
+	}
+	
+	public String getName() {
+		return firstName + " " +lastName;	
+	}
+	
+	public boolean authenticateUser(String id, String passowrd) {
+		if (this.id.equals(id) && this.password.equals(password)) {
+			return true;
+		}else {
+		return false;
+		}
+		
+	}
+	
+	public String displayRole(Object a) {
+		if ( a instanceof Customer ) {
+			return "Customer";
+		}
+		else if ( a instanceof SalesStaff ) {
+			return"SalesStaff";
+		}
+		else if ( a instanceof StoreManager ) {
+			return "StoreManager";
+		}
+		if ( a instanceof WearhouseManager) {
+			return "WearhouseManager";
+		}	
+		else {
+			return "Not an Entiity";
+		}
+		
+	}
+	
 }
