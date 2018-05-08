980f57945f7af385de0cb7d448101f144cabc73f
rmit-s3670659-udahakapugedara-senadhi <37611003+rmit-s3670659-udahakapugedara-senadhi@users.noreply.github.com>
2018-05-08 18:59:40 +1000

    Updated for integration

--- SalesStaff.java
+++ SalesStaff.java
@@ -8,7 +8,7 @@ public class SalesStaff extends Employee {
 	
 	
 	public boolean removeCartItem(Sale sale,SaleLineItem item) {
-		sale.removeItem(item); // this method must be implemented in Sale Class
+		sale.deleteitem(item); // this method must be implemented in Sale Class
 		return true; // for testing, remove later on
 	}
 	
@@ -26,14 +26,20 @@ public class SalesStaff extends Employee {
 	
 	
 	public boolean sellcard(Customer customer,long cardNum, int securityCode, String expDate ){	
-		LoyalityCard card = new LoyalityCard(this,cardNum,securityCode,expDate);
-		customer.addCard(card);
+		//LoyalityCard card = new LoyalityCard(this,cardNum,securityCode,expDate);
+		customer.addCard(this.createCard(this, cardNum, securityCode, expDate));
 		return true;
 		//change expDate type to Date later on
 	}
 	
 	public void displayRole() {
 		System.out.println("SalesStaff");	
+	}
+	
+	public LoyalityCard createCard(SalesStaff sStaff,long cardNum, int securityCode, String expDate) {
+		LoyalityCard card = new LoyalityCard(sStaff,cardNum,securityCode,expDate);
+		return card;
+		
 	}
 		
 
