9d36e7a44abec0afffe92d32fe021be4458409c5
rmit-s3690987-Gaosi-Wang <37578613+rmit-s3690987-Gaosi-Wang@users.noreply.github.com>
2018-05-24 19:32:30 +1000

    Cleaned a bit

--- src/Supermarket.java
+++ src/Supermarket.java
@@ -1,5 +1,6 @@
 /**
  * Supermarket class as main.
+ * :: run the Store - driver class.
  *
  * @author Wan Yi Beh
  * @version 1.0
@@ -9,8 +10,8 @@
 public class Supermarket {
 
    public static void main(String[] args) {
-
       Store store = new Store();
+      
       store.addData();
       store.mainMenu();
 
